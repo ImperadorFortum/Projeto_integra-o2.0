e95dfdb009846d59cb2e59db57eff265b6e7008b
ImperadorFortum <110264843+ImperadorFortum@users.noreply.github.com>
2022-09-09 18:22:42 -0300

    f

--- controller/main_window.py
+++ controller/main_window.py
@@ -32,6 +32,7 @@ class MainWindow(QMainWindow):
             self.addTableWidget(c)
 
     def add(self):
+        id= self.id.text()
         nome = self.nome.text()
         telefone = self.telefone.text()
         data_recebimento = self.data_recebimento.text()
@@ -43,7 +44,8 @@ class MainWindow(QMainWindow):
         novaConsulta.id = id
 
         self.addTableWidget(novaConsulta)
-
+        
+        self.id.clear()
         self.nome.clear()
         self.telefone.clear()
         self.data_recebimento.clear()
@@ -51,12 +53,12 @@ class MainWindow(QMainWindow):
         self.data_entrega.clear()
    
     def edit(self):
-        lineSel = self.tabela.currentRow()
-        n_nome = self.tabela.item(lineSel,0)
-        n_telefone = self.tabela.item(lineSel,1)
-        n_data_recebimento = self.tabela.item(lineSel,2)
-        n_descricao = self.tabela.item(lineSel,3)
-        n_data_entrega = self.tabela.item(lineSel,4)
+        lineSel = self.tabela.currentRow()        
+        n_nome = self.tabela.item(lineSel,1)
+        n_telefone = self.tabela.item(lineSel,2)
+        n_data_recebimento = self.tabela.item(lineSel,3)
+        n_descricao = self.tabela.item(lineSel,4)
+        n_data_entrega = self.tabela.item(lineSel,5)
 
         n_nome = self.nome.text()
         n_telefone = self.telefone.text
@@ -64,7 +66,7 @@ class MainWindow(QMainWindow):
         n_descricao = self.descricao.text
         n_data_entrega = self.data_entrega.text
 
-        edit = Consulta(id,n_nome,n_telefone,n_data_recebimento,n_descricao,n_data_entrega)
+        edit = Consulta(n_nome,n_telefone,n_data_recebimento,n_descricao,n_data_entrega)
            
         self.edicao(edit)  
         ConsultaDAO.edit(edit)
@@ -92,18 +94,19 @@ class MainWindow(QMainWindow):
         line = self.tabela.rowCount()
         self.tabela.insertRow(line)
         
-        nome = QTableWidgetItem(str(c.id))
+        id = QTableWidgetItem(c.id)
+        nome = QTableWidgetItem(c.nome)
         telefone = QTableWidgetItem(c.telefone)
         data_recebimento = QTableWidgetItem(c.data_recebimento)
         descricao = QTableWidgetItem(c.descricao)
         data_entrega= QTableWidgetItem(c.data_entrega)
 
-
-        self.tabela.setItem(line, 0, nome)
-        self.tabela.setItem(line, 1, telefone)
-        self.tabela.setItem(line, 2, data_recebimento)
-        self.tabela.setItem(line, 3, descricao)
-        self.tabela.setItem(line, 4, data_entrega)
+        self.tabela.setItem(line, 0, id)
+        self.tabela.setItem(line, 1, nome)
+        self.tabela.setItem(line, 2, telefone)
+        self.tabela.setItem(line, 3, data_recebimento)
+        self.tabela.setItem(line, 4, descricao)
+        self.tabela.setItem(line, 5, data_entrega)
 
     def edicao(self, c: Editar):   
         lineSel = self.tabela.currentRow()
@@ -113,11 +116,11 @@ class MainWindow(QMainWindow):
         n_descricao = QTableWidgetItem(c.descricao)
         n_data_entrega = QTableWidgetItem(c.data_entrega)
         
-        self.tabela.setItem(lineSel, 0, n_nome)
-        self.tabela.setItem(lineSel, 1, n_telefone)
-        self.tabela.setItem(lineSel, 2, n_data_recebimento)
-        self.tabela.setItem(lineSel, 3, n_descricao)
-        self.tabela.setItem(lineSel, 4, n_data_entrega)
+        self.tabela.setItem(lineSel, 1, n_nome)
+        self.tabela.setItem(lineSel, 2, n_telefone)
+        self.tabela.setItem(lineSel, 3, n_data_recebimento)
+        self.tabela.setItem(lineSel, 4, n_descricao)
+        self.tabela.setItem(lineSel, 5, n_data_entrega)
 
 
 
