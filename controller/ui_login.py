71ed2145bd0b320e0fb2ec0e5e643f181696bb5e
ImperadorFortum <110264843+ImperadorFortum@users.noreply.github.com>
2022-09-27 21:04:36 -0300

    liga

--- controller/ui_login.py
+++ controller/ui_login.py
@@ -8,16 +8,16 @@ class UiLogin(QWidget):
         super(UiLogin,self).__init__()
         uic.loadUi(FILE_UI,self)
 
-        self.entrar.clicked.connect(self.login)
+        self.entrar.clicked.connect(self.Login)
 
-def login(self):
-    login = 'valnormal123'
-    senha = '210998lipe'
-    if login == "login" and senha == "senha" :
-        self.label_4.setText("Entrando...")
-    else :
-        self.label_4.setText("Dados de Login Incorretos!")
-        
+    def Login(self):
+        login = 'usuario'
+        senha = 'senha'
+        if login == "login" and senha == "senha" :
+            self.label_4.setText("Entrando...")
+        else :
+            self.label_4.setText("Dados de Login Incorretos!")
+            
        
 
 
